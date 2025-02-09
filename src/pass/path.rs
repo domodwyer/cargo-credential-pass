d5c37498673cd9196ee11c3e6c7ad7e443510160
Dom Dwyer <dom@itsallbroken.com>
2025-02-09 14:15:39 +0100

    refactor: add PassPathBuilder to init PassPath
    Use a builder to construct the PassPath and support specifying the
    relative directory under which the token will be stored (defaulting to
    the existing "cargo-registry").

--- src/pass/path.rs
+++ src/pass/path.rs
@@ -1,19 +1,51 @@
 use std::{
     ffi::{OsStr, OsString},
     fmt::Display,
+    path::{Path, PathBuf},
 };
 
 use cargo_credential::RegistryInfo;
 
-/// A `pass` path used to deterministically address a specific registry.
-#[derive(Debug)]
-pub(crate) struct PassPath(OsString);
+/// An initialiser of [`PassPath`] from configuration.
+#[derive(Debug, Default)]
+pub(crate) struct PassPathBuilder<'a, 'b> {
+    /// Directory to store token relative to password store root.
+    dir: Option<&'a Path>,
 
-impl PassPath {
-    /// Construct a [`PassPath`] from the provided `name` if [`Some`], or
-    /// deterministically derive one from `index_url` if [`None`].
-    pub(crate) fn new(index_url: &str, name: Option<&str>) -> Self {
-        let name = name.map(ToString::to_string).unwrap_or_else(|| {
+    /// Filename without extension.
+    name: Option<&'b str>,
+}
+
+impl<'a, 'b> PassPathBuilder<'a, 'b> {
+    /// Set the path relative to the password store root, under which the token
+    /// is created.
+    ///
+    /// If not provided, tokens will be stored under the default path of
+    /// `cargo-registry`.
+    ///
+    /// # Panics
+    ///
+    /// This call panics if an absolute path is provided. A relative path must
+    /// be provided as the path will be interpreted relative to the password
+    /// store root.
+    #[expect(dead_code)]
+    pub(crate) fn under_dir(mut self, dir: &'a Path) -> Self {
+        assert!(dir.is_relative());
+
+        self.dir = Some(dir);
+        self
+    }
+
+    /// Set the name of the token, or deterministically derive it from the
+    /// registry URL if not provided.
+    pub(crate) fn with_name(mut self, name: &'b str) -> Self {
+        self.name = Some(name);
+        self
+    }
+
+    /// Instantiate a [`PassPath`] with the specified components.
+    pub(crate) fn build(self, index_url: &str) -> PassPath {
+        let name = self.name.map(ToString::to_string).unwrap_or_else(|| {
             // Remove the sparse+ prefix from the URL if present, so that a
             // token is resolved for a registry regardless of the index protocol
             // used.
@@ -21,7 +53,33 @@ impl PassPath {
             normalise_url(name)
         });
 
-        let path = format!("cargo-registry/{}.token", name);
+        let mut path = self.dir.map(ToOwned::to_owned).unwrap_or_else(|| {
+            let mut p = PathBuf::new();
+            p.push("cargo-registry");
+            p
+        });
+
+        path.push(format!("{}.token", name));
+
+        PassPath::new(path)
+    }
+}
+
+/// A `pass` path used to deterministically address a specific registry.
+#[derive(Debug)]
+pub(crate) struct PassPath(OsString);
+
+impl PassPath {
+    /// Initialise a new [`PassPath`] instance that stores the token in the
+    /// specified path.
+    ///
+    /// # Panics
+    ///
+    /// This call panics if an absolute path is provided. A relative path must
+    /// be provided as the path will be interpreted relative to the password
+    /// store root.
+    pub(crate) fn new(path: PathBuf) -> Self {
+        assert!(path.is_relative());
 
         Self(OsString::from(path))
     }
@@ -41,7 +99,14 @@ impl AsRef<OsStr> for PassPath {
 
 impl<'a> From<&'a RegistryInfo<'a>> for PassPath {
     fn from(v: &'a RegistryInfo<'a>) -> Self {
-        PassPath::new(v.index_url, v.name)
+        let mut p = PassPathBuilder::default();
+
+        // Use the provided name or infer one if not specified.
+        if let Some(name) = v.name {
+            p = p.with_name(name);
+        }
+
+        p.build(v.index_url)
     }
 }
 
@@ -56,17 +121,16 @@ mod tests {
 
     #[test]
     fn test_path_with_name() {
-        let p = PassPath::new(
-            "sparse+https://cargo.itsallbroken.com/index/",
-            Some("bananas"),
-        );
+        let p = PassPathBuilder::default()
+            .with_name("bananas")
+            .build("sparse+https://cargo.itsallbroken.com/index/");
 
         assert_eq!(p.to_string(), "cargo-registry/bananas.token");
     }
 
     #[test]
     fn test_path_without_name_sparse() {
-        let p = PassPath::new("sparse+https://cargo.itsallbroken.com/index/", None);
+        let p = PassPathBuilder::default().build("sparse+https://cargo.itsallbroken.com/index/");
 
         assert_eq!(
             p.to_string(),
@@ -76,11 +140,42 @@ mod tests {
 
     #[test]
     fn test_path_without_name_non_sparse() {
-        let p = PassPath::new("https://cargo.itsallbroken.com/index/", None);
+        let p = PassPathBuilder::default().build("https://cargo.itsallbroken.com/index/");
 
         assert_eq!(
             p.to_string(),
             "cargo-registry/https___cargo_itsallbroken_com_index_.token"
         );
+    }
+
+    #[test]
+    fn test_path_under_dir() {
+        let p = PassPathBuilder::default()
+            .with_name("bananas")
+            .under_dir(Path::new("platanos/are/good"))
+            .build("sparse+https://cargo.itsallbroken.com/index/");
+
+        assert_eq!(p.to_string(), "platanos/are/good/bananas.token");
+    }
+
+    #[test]
+    fn test_path_under_dir_trailing_slash() {
+        let p = PassPathBuilder::default()
+            .with_name("bananas")
+            .under_dir(Path::new("platanos/are/good/"))
+            .build("sparse+https://cargo.itsallbroken.com/index/");
+
+        assert_eq!(p.to_string(), "platanos/are/good/bananas.token");
+    }
+
+    #[should_panic(expected = "dir.is_relative()")]
+    #[test]
+    fn test_path_under_absolute_dir() {
+        let p = PassPathBuilder::default()
+            .with_name("bananas")
+            .under_dir(Path::new("/root"))
+            .build("sparse+https://cargo.itsallbroken.com/index/");
+
+        assert_eq!(p.to_string(), "platanos/are/good/bananas.token");
     }
 }
